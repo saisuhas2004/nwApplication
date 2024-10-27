5ffa53069b92d87a932c6618bb0695c5e58ddd47
SreePavan <spavankumar30@gmail.com>
2024-10-27 18:53:13 -0400

    Multiple Browsers

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,6 +2,19 @@ from selenium import    webdriver
 import pytest
 
 @pytest.fixture()
-def setup():
-    driver=webdriver.Chrome()
+def setup(browser):
+    if browser == 'chrome':
+        driver=webdriver.Chrome()
+        print("launching Chrome Browser")
+    elif browser == 'firefox':
+         driver=webdriver.Firefox()
+         print("launching Firefox Browser")
     return driver
+
+
+def pytest_addoption(parser):  #This will get the value from CLI/Hooks
+   parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request): # This will return the browser value to set up method
+    return request.config.getoption("--browser")
