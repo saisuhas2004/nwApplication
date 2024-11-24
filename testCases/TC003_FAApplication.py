a2f6c557bab789e4f11af6d12e8a018a66026e89
SreePavan <spavankumar30@gmail.com>
2024-11-24 15:01:49 -0500

    Non FA Application

--- testCases/TC003_FAApplication.py
+++ testCases/TC003_FAApplication.py
@@ -1,12 +1,3 @@
-import random
-import time
-from webbrowser import Chrome
-
-import pytest
-from selenium import webdriver
-
-import pageObjects
-
 from pageObjects.LoginPage import LoginPage
 from pageObjects.accountHomePage import accountHomePage
 from pageObjects.homeAddress import homeAddress
@@ -26,28 +17,23 @@ from utilities.customLogger import LogGen
 from utilities.commonMethods import CommonMethods
 from utilities.writeToOracleDatabase import writeToOracleDB
 
-class Test_002_NonFAApplication:
+class Test_003_FAApplication:
     baseURL=ReadConfig.getApplicationURL()
     username=ReadConfig.getUserName()
     password=ReadConfig.getPassword()
     logger=LogGen.loggen()
     Commonmethods = CommonMethods
+
     print("Email Retrived from Database")
     print(writeToOracleDB.readFromDatabase())
 
-
-
-    def test_NonFAApplication(self, setup):
+    def test_FAApplication(self, setup):
         State = 'DE'
         self.logger.debug("Test Case_01 HomePage Title")
         self.driver = setup
         self.driver.get(self.baseURL)
         act_title = self.driver.title
         print(act_title)
-        # start the application
-        # accountHomePage.selectStateOnAccountHomePage(self)
-        # protectingYourPersonalInformation.protectingYourPersonalInformation(self)
-        # yourMarketPlaceApplicationPage.ContinueToApplication(self)
         self.logger.debug("Test Case_01 HomePage Title")
         self.driver = setup
         self.driver.get(self.baseURL)
@@ -56,17 +42,10 @@ class Test_002_NonFAApplication:
         LoginPage.closePopupOnLandingPage(self)
         LoginPage.clickOn_LoginLink(self)
         LoginPage.LoginWithExistingAccount_HealthCare(self, writeToOracleDB.readFromDatabase())
-       #start the application
+
+        # start the application
         accountHomePage.selectStateOnExistingAccountHomePage(self, State)
         protectingYourPersonalInformation.protectingYourPersonalInformation(self)
         yourMarketPlaceApplicationPage.continueYourMarketPlaceApplicationPage(self)
         applicationSetupPage.applicationSetupPageContinue(self)
         decideToCheckForSavings.enterApplicationTaxFilingInformation(self)
-        tellUsAboutYourself.tellUsAboutYourselfInformation(self)
-        homeAddress.homeAddressInfo(self,State)
-        whoNeedsHealthCoverage.whoNeedHealthCoverage(self)
-        currentCoverageAndSEP.currentCoverage(self)
-        currentCoverageAndSEP.specialEnrollmentPeriodEligibility(self)
-        currentCoverageAndSEP.healthReimbursementArrangementHRAOffers(self)
-        currentCoverageAndSEP.specialEnrollmentPage(self)
-        planComparePage.eligibleToEnroll(self)
